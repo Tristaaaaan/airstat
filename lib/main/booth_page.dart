8c298e15466bcfe845a5b160e7f1e7e426d0b327
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-07-15 19:27:01 +0800

    checking raw data

--- lib/main/booth_page.dart
+++ lib/main/booth_page.dart
@@ -1,88 +1,264 @@
-// import 'dart:developer';
+import 'dart:async';
+import 'dart:typed_data';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_hooks/flutter_hooks.dart';
-// import 'package:flutter_serial/flutter_serial.dart';
-// import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:airstat/components/snackbar/information_snackbar.dart';
+import 'package:flutter/material.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
 
-// // State for log data and received data
-// final logDataProvider = StateProvider<String>((ref) => "");
-// final receivedDataProvider = StateProvider<String>((ref) => "");
-// final selectedPortProvider = StateProvider<String>((ref) => "Selected Port");
-// final selectedBaudRateProvider =
-//     StateProvider<int>((ref) => FlutterSerial().baudRateList.first);
-// final serialListProvider = StateProvider<List<String>>((ref) => []);
+class BoothPage extends StatefulWidget {
+  const BoothPage({super.key});
 
-// final formatProvider = StateProvider<DataFormat>((ref) => DataFormat.ASCII);
-// final messageProvider = StateProvider<String>((ref) => "");
-// final serialCommunicationProvider =
-//     Provider<FlutterSerial>((ref) => FlutterSerial());
+  @override
+  _BoothPage createState() => _BoothPage();
+}
 
-// class Booth extends HookConsumerWidget {
-//   const Booth({super.key});
+class _BoothPage extends State<BoothPage> {
+  UsbPort? _port;
+  String _status = "Idle";
 
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final logData = ref.watch(logDataProvider);
-//     final receivedData = ref.watch(receivedDataProvider);
-//     final selectedPort = ref.watch(selectedPortProvider);
-//     final selectedBaudRate = ref.watch(selectedBaudRateProvider);
-//     final serialList = ref.watch(serialListProvider);
-//     final format = ref.watch(formatProvider);
+  final List<String> _serialData = [];
+  final List<UsbDevice> _devices = [];
+  final List<String> _logs = [];
+  StreamSubscription<Uint8List>? _subscription;
+  Transaction<String>? _transaction;
 
-//     final serialComm = ref.watch(serialCommunicationProvider);
+  // Future<bool> _connectTo(UsbDevice? device) async {
+  //   _serialData.clear();
 
-//     useEffect(() {
-//       final subscription = serialComm.startSerial().listen((result) {
-//         ref.read(logDataProvider.notifier).state = result.logChannel ?? "";
-//         ref.read(receivedDataProvider.notifier).state =
-//             result.readChannel ?? "";
-//         log('Log Data: $logData');
-//         log('Received Data: $receivedData');
-//       });
+  //   try {
+  //     if (_subscription != null) {
+  //       await _subscription!.cancel();
+  //       _subscription = null;
+  //     }
 
-//       getSerialList(ref);
+  //     if (_transaction != null) {
+  //       _transaction!.dispose();
+  //       _transaction = null;
+  //     }
 
-//       return subscription.cancel;
-//     }, []);
+  //     if (_port != null) {
+  //       await _port!.close();
+  //       _port = null;
+  //     }
 
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Serial Communication Checker'),
-//         centerTitle: true,
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(8.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             if (serialList.isEmpty)
-//               const Center(
-//                 child: Text("No Serial Ports"),
-//               ),
-//             if (serialList.isNotEmpty)
-//               ListView(
-//                 shrinkWrap: true,
-//                 children: serialList
-//                     .map((port) => ListTile(
-//                           title: Text(port),
-//                         ))
-//                     .toList(),
-//               )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  //     if (device == null) {
+  //       setState(() {
+  //         _status = "Disconnected";
+  //       });
+  //       _logs.add("Disconnected");
+  //       return true;
+  //     }
 
-// void getSerialList(WidgetRef ref) async {
-//   final serialComm = ref.watch(serialCommunicationProvider);
-//   try {
-//     final ports = await serialComm.getAvailablePorts();
-//     ref.read(serialListProvider.notifier).state = ports!;
-//     log('Serial Ports: $ports');
-//   } catch (e) {
-//     log('Error fetching serial ports: $e');
-//   }
-// }
+  //     _port = await device.create();
+  //     if (await (_port!.open()) != true) {
+  //       setState(() {
+  //         _status = "Failed to open port";
+  //       });
+  //       _logs.add("Failed to open port");
+  //       return false;
+  //     }
+
+  //     await _port!.setDTR(true);
+  //     await _port!.setRTS(true);
+  //     await _port!.setPortParameters(
+  //         9600,
+  //         UsbPort.DATABITS_8,
+  //         UsbPort.STOPBITS_1,
+  //         UsbPort.PARITY_NONE); // Ensure these match your device's settings
+
+  //     _transaction = Transaction.stringTerminated(
+  //         _port!.inputStream as Stream<Uint8List>,
+  //         Uint8List.fromList([13, 10]));
+
+  //   _subscription = _port!.inputStream.listen((Uint8List data) {
+  //     setState(() {
+  //       _logs.add("Received raw data: ${data.toString()}");
+  //       _serialData.add(String.fromCharCodes(data));
+  //       if (_serialData.length > 20) {
+  //         _serialData.removeAt(0);
+  //       }
+
+  //       if (_logs.length > 100) {
+  //         _logs.removeAt(0);
+  //       }
+  //     });
+  //   });
+
+  //     _subscription = _transaction!.stream.listen((String line) {
+  //       setState(() {
+  //         // Add the received line to _serialData
+  //         _serialData.add(line);
+  //         if (_serialData.length > 20) {
+  //           _serialData.removeAt(0);
+  //         }
+
+  //         // Add the received line to _logs
+  //         _logs.add(line);
+  //         if (_logs.length > 100) {
+  //           // Keep the log size manageable
+  //           _logs.removeAt(0);
+  //         }
+  //       });
+  //     });
+
+  //     setState(() {
+  //       _status = "Connected";
+  //     });
+  //     _logs.add("Connected");
+  //     return true;
+  //   } catch (e) {
+  //     // Log any error that occurs during the connection process
+  //     setState(() {
+  //       _status = "Error: ${e.toString()}";
+  //     });
+  //     _logs.add("Error: ${e.toString()}");
+  //     return false;
+  //   }
+  // }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  //   _connectTo(null);
+  // }
+
+  Future<bool> _connectTo(UsbDevice? device) async {
+    _serialData.clear();
+
+    try {
+      if (_subscription != null) {
+        await _subscription!.cancel();
+        _subscription = null;
+      }
+
+      if (_transaction != null) {
+        _transaction!.dispose();
+        _transaction = null;
+      }
+
+      if (_port != null) {
+        await _port!.close();
+        _port = null;
+      }
+
+      if (device == null) {
+        setState(() {
+          _status = "Disconnected";
+        });
+        _logs.add("Disconnected");
+        return true;
+      }
+
+      _port = await device.create();
+      if (await (_port!.open()) != true) {
+        setState(() {
+          _status = "Failed to open port";
+        });
+        _logs.add("Failed to open port");
+        return false;
+      }
+
+      await _port!.setDTR(true);
+      await _port!.setRTS(true);
+      await _port!.setPortParameters(
+          9600, // Check the correct baud rate for WindSonic
+          UsbPort.DATABITS_8,
+          UsbPort.STOPBITS_1,
+          UsbPort.PARITY_NONE); // Ensure these match your device's settings
+
+      _subscription = _port!.inputStream!.listen((Uint8List data) {
+        setState(() {
+          _logs.add("Received raw data: ${data.toString()}");
+          String receivedData = String.fromCharCodes(data);
+          _serialData.add(receivedData);
+          if (_serialData.length > 20) {
+            _serialData.removeAt(0);
+          }
+
+          if (_logs.length > 100) {
+            _logs.removeAt(0);
+          }
+        });
+      });
+
+      setState(() {
+        _status = "Connected";
+      });
+      _logs.add("Connected");
+      return true;
+    } catch (e) {
+      setState(() {
+        _status = "Error: ${e.toString()}";
+      });
+      _logs.add("Error: ${e.toString()}");
+      return false;
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('USB Serial Plugin example app'),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                onPressed: () async {
+                  List<UsbDevice> serialList = await UsbSerial.listDevices();
+
+                  setState(() {
+                    _devices.clear();
+                    _devices.addAll(serialList);
+                  });
+                },
+                child: const Text(
+                  "Check Serial Connection",
+                ),
+              ),
+              if (_devices.isNotEmpty)
+                SizedBox(
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: _devices.length,
+                    itemBuilder: (context, index) {
+                      final UsbDevice device = _devices[index];
+                      return ListTile(
+                        leading: const Icon(Icons.usb),
+                        title: Text(device.deviceId.toString()),
+                        subtitle: Text(device.manufacturerName ?? ''),
+                        onTap: () async {
+                          await _connectTo(device);
+                          if (context.mounted) {
+                            informationSnackBar(
+                                context, Icons.usb, "Connecting...");
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ),
+              Text('Status: $_status\n'),
+              Text('info: ${_port.toString()}\n'),
+              if (_serialData.isNotEmpty)
+                Text("RESULT: ${_serialData.join('\n')}\n"),
+              Text("RESULT: ${_serialData.join('\n')}\n",
+                  style: Theme.of(context).textTheme.titleLarge),
+              if (_logs.isNotEmpty) Text("RESULT: ${_logs.join('\n')}\n"),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
--- lib/main/homepage.dart
+++ lib/main/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:airstat/components/container/menu_container.dart';
+import 'package:airstat/main/booth_page.dart';
+import 'package:airstat/main/continuous_page.dart';
 import 'package:airstat/main/file_page.dart';
 import 'package:airstat/main/settings.dart';
 import 'package:airstat/main/three_d_page.dart';
@@ -20,9 +22,16 @@ class Home extends StatelessWidget {
                   crossAxisCount: 4,
                 ),
                 children: [
-                  const MenuContainer(
+                  MenuContainer(
                     image: 'assets/icons/Icon_booth_orange.png',
                     label: 'Booth',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const BoothPage()),
+                      );
+                    },
                   ),
                   MenuContainer(
                     image: 'assets/icons/Icon3dMap.png',
@@ -35,9 +44,16 @@ class Home extends StatelessWidget {
                       );
                     },
                   ),
-                  const MenuContainer(
+                  MenuContainer(
                     image: 'assets/icons/Icon_continuous_orange.png',
                     label: 'Continuous',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Continuous()),
+                      );
+                    },
                   ),
                   MenuContainer(
                     image: 'assets/icons/Icon_files_orange.png',
