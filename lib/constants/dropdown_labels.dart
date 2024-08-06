5e929ca304e559dac9b3ed120fb9999e80f4b9ea
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-08-06 20:45:03 +0800

    fix booth indexing

--- lib/constants/dropdown_labels.dart
+++ lib/constants/dropdown_labels.dart
@@ -31,7 +31,15 @@ enum RowLabels {
   nine('9'),
   ten('10'),
   eleven('11'),
-  twelve('12');
+  twelve('12'),
+  thirteen('13'),
+  fourteen('14'),
+  fifteen('15'),
+  sixteen('16'),
+  seventeen('17'),
+  eighteen('18'),
+  nineteen('19'),
+  twenty('20');
 
   const RowLabels(this.label);
 
